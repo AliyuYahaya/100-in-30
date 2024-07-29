fc798ff8f031155f580deaf941f31b32f62e2c12
Aliyu Yahaya <st10440112@vcconnect.edu.za>
2024-07-29 13:40:08 +0200

    P7 DONE
    p7 is done

--- Project-7_FactorialCalculator/src/Main.java
+++ Project-7_FactorialCalculator/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        menu();
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Calculator");
+        System.out.println("Enter 1 to calculate the factorial of a number or press 0 to exit");
+        int choice = sc.nextInt();
+        while (choice == 1) {
+            System.out.println("Enter the number to calculate the factorial");
+            int number = sc.nextInt();
+            calculateFactorial(number);
+            System.out.println("Enter 1 to calculate the factorial of a number or press 0 to exit");
+            choice = sc.nextInt();
+        }
+
+    }
+    public static void calculateFactorial(int number) {
+        int factorialNumber = number;
+        for (int i = 1; i < number; i++) {
+            factorialNumber = factorialNumber * i;
+        }
+        System.out.println(factorialNumber);
     }
 }
