a923eb3c72f026d3d6957924f47577eabe111fe3
Aliyu Yahaya <st10440112@vcconnect.edu.za>
2024-07-30 12:35:27 +0200

    P10

---
+++ Project-10_SimpleTodo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init() {}
+
+    public static void menu() {}
+
+    public static void addTask() {}
+    public static void removeTask() {}
+    public static void showTasks() {}
+}
